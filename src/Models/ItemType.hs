cf08e8a174d9a0fc72542327945185e8dfa92f66
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2024-05-12 19:14:58 -0500

    ItemType should conform to Enum and Bounded
    This will let us use it easily in things like selectField

--- src/Models/ItemType.hs
+++ src/Models/ItemType.hs
@@ -4,5 +4,5 @@ module Models.ItemType where
 import           Database.Persist.TH
 
 data ItemType = Pin | Patch
-    deriving (Show, Read, Eq)
+    deriving (Show, Read, Eq, Enum, Bounded)
 derivePersistField "ItemType"
